1e007531e49cd44f64e9f8f9929080b34fecfa7f
Lucas Bastos <lucsbasto@gmail.com>
2019-04-25 10:33:43 -0300

    melhorando o código do img-extrator.py

--- img-extrator.py
+++ img-extrator.py
@@ -1,32 +1,25 @@
 def getType(img):
-    print (img)
-    if (img == '6677'):
+    if (img == '6677' or img == b'BM'):
         print ("Imagem: BMP - Identificação: Sim")
     else:
         print ("Imagem: null - Identificação: Não")
 
 
-def get_h_w(bytes):
-    w = 0
-    h = 0
-    for b in range(len(bytes)):
-        if (b >= 18 and b < 22):
-            h += bytes[b]
-        if (b >= 22 and b < 26):
-            w += bytes[b]
-
-    print("height: ", h)
-    print("width: ", w)
-
-img = open('imagem.bmp', 'rb')
-img_big = open('The_death.bmp', 'rb')
-img_big_read = img_big.read()
-img_read = img.read()
-two_pos = str(img_read[0]) + str(img_read[1])
-two_pos = str(img_big_read[0]) + str(img_big_read[1])
-
-getType(two_pos) #passar as duas primeiras posições
-get_h_w(bytearray(img_big_read)) #passar a o imagem.read()
+def get_height_width(bytes):
+    height = img[18]
+    width = img[22]
+    print("height: ", height)
+    print("width: ", width)
 
 
-#funciona apenas para imagens com altura e largura com até 255 px
+file = open('imagem.bmp', 'rb')
+img = file.read()
+size = len(img)
+type = img[:2]
+header = img[:14]
+
+print (size)
+getType(type)
+get_height_width(img)
+
+#funciona apenas para imagem com heigth e width menores que 256
