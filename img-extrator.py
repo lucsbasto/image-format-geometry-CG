b8e46b2c1ffa44d2f5761bb77bf477016d352517
Lucas Bastos <lucsbasto@gmail.com>
2019-04-24 21:42:34 -0300

    Add files via upload

---
+++ img-extrator.py
@@ -0,0 +1,29 @@
+def getType(img):
+    print (img)
+    if (img == '6677'):
+        print ("Imagem: BMP - Identificação: Sim")
+    else:
+        print ("Imagem: null - Identificação: Não")
+
+
+def get_h_w(bytes):
+    w = 0
+    h = 0
+    for b in range(len(bytes)):
+        if (b >= 18 and b < 22):
+            w += bytes[b]
+        if (b >= 22 and b < 26):
+            h += bytes[b]
+
+    print("height: ", h)
+    print("width: ", w)
+
+img = open('imagem.bmp', 'rb')
+img_big = open('The_death.bmp', 'rb')
+img_big_read = img_big.read()
+img_read = img.read()
+two_pos = str(img_read[0]) + str(img_read[1])
+two_pos = str(img_big_read[0]) + str(img_big_read[1])
+
+getType(two_pos) #passar as duas primeiras posições
+get_h_w(bytearray(img_big_read)) #passar a o imagem.read()
